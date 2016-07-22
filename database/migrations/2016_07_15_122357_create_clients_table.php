e1387e0a17610ee35abfa6aa28bf603d3b747877
mtoto.mgonjwa@gmail.com <0001@Wamatu>
2016-07-22 16:40:46 +0300

    We have now integrated the Google Maps API Markers are shown on the Google maps and the show street-view if need be, we also have to have a default image if it does'nt exist. Button click displays latitude and longitude. We need to make the data on the alert box be display on the add site form

---
+++ database/migrations/2016_07_15_122357_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('client_name');
+            $table->text('client_contact');
+            $table->string('client_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
